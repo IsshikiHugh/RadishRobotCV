ad34bbe63c928029fa179579d2ea08b84c0d8f08
Isshiki_Hugh <2511524185@qq.com>
2022-05-21 13:38:33 +0800

    new

---
+++ initialize.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/PaddlePaddle/PaddleDetection.git
+git clone https://github.com/thorn-jmh/3dbaseline.git
+
+cp "./tmpFile/output_inference/" "./PaddleDetection/output_inference/" -r
